4839da8e7e6283dd11ec9d2d61560a89be2797d6
Supriyo Mahato <mesupriyomahato@gmail.com>
2025-05-30 17:51:45 +0530

    made changes in avatar img

--- src/app/(account)/account/laoder.tsx
+++ src/app/(account)/account/laoder.tsx
@@ -1,6 +1,6 @@
 "use client";
 import NavBar from "@/components/molecules/navbar/navbar";
-import styles from './account.module.scss'
+import styles from './loader.module.scss'
 import { motion } from "framer-motion";
 
 export default function Loading() {
@@ -10,15 +10,16 @@ export default function Loading() {
       <div className={styles.container}>
         <div className={styles.header}>
           <div className={styles.headerdata}>
+            <span
+              className={styles.avatar}>
+            </span>
             <motion.h1
               initial={{ opacity: 0, y: -20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.3, ease: [0.785, 0.135, 0.15, 0.86] }}
               className={styles.title}>Account settings</motion.h1>
           </div>
-          <div
-            className={styles.avatar}>
-          </div>
+
 
         </div>
 

85c5d574f1355d0967fe16b176c9b8ed731836a1
Supriyo Mahato <mesupriyomahato@gmail.com>
2025-05-31 21:27:47 +0530

    made changes

---
+++ src/app/payment/history/loading.tsx
@@ -0,0 +1,25 @@
+"use client"
+import { motion } from 'framer-motion';
+import styles from './loading.module.scss';
+
+export default function Loading() {
+    return (
+        <div className={styles.wraper}>
+            <div className={styles.container}>
+                <div className={styles.header}>
+                    <motion.h1
+                        initial={{ opacity: 0, y: -20 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.3, ease: [0.785, 0.135, 0.15, 0.86] }}
+                        className={styles.title}>
+                        Payment History
+                    </motion.h1>
+                </div>
+
+                <div className={styles.paymentwraper}>
+
+                </div>
+            </div>
+        </div>
+    )
+}

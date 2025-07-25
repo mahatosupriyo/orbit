c9f00e6edeb66f9555c1623f4c3aa5f1d721d4b2
Supriyo Mahato <mesupriyomahato@gmail.com>
2025-07-25 21:23:19 +0530

    connmit changes

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -72,9 +72,13 @@ export default async function UserPage({ params }: UserPageProps) {
           <div className={styles.userProfile}>
             <AvatarImageForUser userId={user.id} size={100} />
             <div className={styles.userInfo}>
+              <svg xmlns="http://www.w3.org/2000/svg" className={styles.orbitoriginal} viewBox="0 0 138 231" fill="none">
+                <path d="M0 0H138V231L69 208.5L0 231V0Z" fill="#1414FF" />
+                <path fillRule="evenodd" clipRule="evenodd" d="M69.1426 135.861C73.1293 135.861 76.3611 132.629 76.3611 128.643C76.3611 124.656 73.1293 121.424 69.1426 121.424C65.1559 121.424 61.924 124.656 61.924 128.643C61.924 132.629 65.1559 135.861 69.1426 135.861ZM69.1426 176.285C95.4548 176.285 116.785 154.955 116.785 128.643C116.785 102.33 95.4548 81 69.1426 81C42.8303 81 21.5 102.33 21.5 128.643C21.5 154.955 42.8303 176.285 69.1426 176.285Z" fill="white" />
+              </svg>
               {user.username && <p className={styles.userUsername}>
                 {user.username}
-                <Icon name="verified" size={11.6}/>
+                <Icon name="verified" size={11.6} />
               </p>}
               {user.name && (
                 <h1 className={styles.userName}>

a2eb50b35f60938e545bf33cf17b8bb8d57276bd
Supriyo Mahato <mesupriyomahato@gmail.com>
2025-07-26 15:51:10 +0530

    added loader in username

---
+++ src/app/[username]/loading.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import NavBar from "@/components/molecules/navbar/navbar";
+import BackBtn from "@/components/atoms/(buttons)/backbtn/backbtn";
+import ShimmerLoader from "@/components/atoms/loading/loadingbox";
+import styles from "./username.module.scss";
+
+export default function Loading() {
+    return (
+        <div className={styles.wraper}>
+            <NavBar />
+            <div className={styles.container}>
+                <BackBtn />
+
+                {/* Avatar + Username */}
+                <div className={styles.userProfile}>
+                    <ShimmerLoader height="100px" width="100px" borderRadius="50rem" />
+                </div>
+
+                {/* Posts */}
+                <div className={styles.postsSection}>
+                    <h2 className={styles.sectionTitle}></h2>
+                    <div className={styles.gridpostlayout}>
+                        {Array.from({ length: 3 }).map((_, i) => (
+                            <ShimmerLoader
+                                key={i}
+                                className={styles.dropcard}
+                                height="100%"
+                                width="100%"
+                                aspect-ratio="4/5"
+                                borderRadius="1.6rem"
+                            />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}

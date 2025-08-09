43e438119a3b4e709cd5bf806e3dcc7e4f09eeba
Supriyo Mahato <mesupriyomahato@gmail.com>
2025-08-09 22:14:12 +0530

    uipdatedsearch

--- src/app/astra/astramaincomponent.tsx
+++ src/app/astra/astramaincomponent.tsx
@@ -70,9 +70,14 @@ export default function AstraSearch() {
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.2, ease: [0.785, 0.135, 0.15, 0.86] }}
-                onSubmit={handleSubmit} 
+                onSubmit={handleSubmit}
                 className={styles.inputBox}
-                >
+            >
+
+                <svg xmlns="http://www.w3.org/2000/svg" className={styles.searchicon} viewBox="0 0 39 39">
+                    <path d="M25.8762 23.8695C27.1206 22.2102 27.8545 20.1538 27.8545 17.9273C27.8545 12.4531 23.4015 8 17.9273 8C12.4531 8 8 12.4531 8 17.9273C8 23.4015 12.4531 27.8545 17.9273 27.8545C20.1538 27.8545 22.2102 27.1171 23.8695 25.8762L28.2694 30.2761C28.5459 30.5526 28.9075 30.6909 29.2727 30.6909C29.6379 30.6909 29.9995 30.5526 30.2761 30.2761C30.8292 29.723 30.8292 28.826 30.2761 28.2694L25.8762 23.8695ZM17.9273 25.0182C14.0166 25.0182 10.8364 21.8379 10.8364 17.9273C10.8364 14.0166 14.0166 10.8364 17.9273 10.8364C21.8379 10.8364 25.0182 14.0166 25.0182 17.9273C25.0182 21.8379 21.8379 25.0182 17.9273 25.0182Z" />
+                </svg>
+
                 <input
                     type="text"
                     className={styles.input}
@@ -81,20 +86,10 @@ export default function AstraSearch() {
                     onChange={handleInputChange}
                     disabled={isPending}
                 />
-                <button
-                    type="submit"
-                    disabled={isPending}
-                    className={styles.astrabutton}
-                >
 
-                    <svg xmlns="http://www.w3.org/2000/svg" className={styles.searchicon} viewBox="0 0 39 39">
-                        <path d="M25.8762 23.8695C27.1206 22.2102 27.8545 20.1538 27.8545 17.9273C27.8545 12.4531 23.4015 8 17.9273 8C12.4531 8 8 12.4531 8 17.9273C8 23.4015 12.4531 27.8545 17.9273 27.8545C20.1538 27.8545 22.2102 27.1171 23.8695 25.8762L28.2694 30.2761C28.5459 30.5526 28.9075 30.6909 29.2727 30.6909C29.6379 30.6909 29.9995 30.5526 30.2761 30.2761C30.8292 29.723 30.8292 28.826 30.2761 28.2694L25.8762 23.8695ZM17.9273 25.0182C14.0166 25.0182 10.8364 21.8379 10.8364 17.9273C10.8364 14.0166 14.0166 10.8364 17.9273 10.8364C21.8379 10.8364 25.0182 14.0166 25.0182 17.9273C25.0182 21.8379 21.8379 25.0182 17.9273 25.0182Z" />
-                    </svg>
-                    <span className={styles.key}>
-                        ↵
-                    </span>
-                    {/* {isPending ? 'Searching...' : 'Search'} */}
-                </button>
+                <span className={styles.key} style={{marginRight: '2rem'}}>
+                    ↵
+                </span>
             </motion.form>
 
             {pathname === '/astra' && results.length === 0 && !isPending && (

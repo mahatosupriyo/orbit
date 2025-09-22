86bc4fb25b4ee221c0a44c049e89c87d3d606423
Supriyo Mahato <mesupriyomahato@gmail.com>
2025-09-22 22:45:54 +0530

    aded

--- src/app/(main)/garage/garagefeed.tsx
+++ src/app/(main)/garage/garagefeed.tsx
@@ -52,10 +52,9 @@ export default function GarageFeed({ posts }: { posts: GaragePost[] }) {
         <div className={styles.capsulegrid}>
 
           <div className={styles.gridhead}>
-            <motion.div
-              whileTap={{ opacity: 0.4 }}
-              className={styles.gridcapsule}>
-              <Link draggable="false" className={styles.linkwraper} href="/colorextractor" aria-label="Color">
+            <Link draggable="false" className={styles.linkwraper} href="/colorextractor" aria-label="Color">
+              <div
+                className={styles.gridcapsule}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" viewBox="0 0 46 46" fill="none">
                   <path d="M24.8478 10.0631C24.8478 9.04192 24.0207 8.21484 22.9996 8.21484C21.9784 8.21484 21.1514 9.04192 21.1514 10.0631V12.8354C21.1514 13.8565 21.9784 14.6836 22.9996 14.6836C24.0207 14.6836 24.8478 13.8565 24.8478 12.8354V10.0631Z" fill="white" />
                   <path d="M16.9143 14.3001L15.0661 12.4519C14.3453 11.7311 13.1717 11.7311 12.4509 12.4519C11.7301 13.1727 11.7301 14.3417 12.4509 15.0671L14.2991 16.9153C14.6595 17.2757 15.1308 17.4559 15.6067 17.4559C16.0826 17.4559 16.5539 17.2757 16.9143 16.9153C17.6351 16.1945 17.6351 15.0255 16.9143 14.3001Z" fill="white" />
@@ -66,8 +65,8 @@ export default function GarageFeed({ posts }: { posts: GaragePost[] }) {
                   <path d="M35.9371 24.8626C36.9582 24.8626 37.7853 24.0356 37.7853 23.0144C37.7853 21.9933 36.9582 21.1662 35.9371 21.1662H33.1648C32.1436 21.1662 31.3165 21.9933 31.3165 23.0144C31.3165 24.0356 32.1436 24.8626 33.1648 24.8626H35.9371Z" fill="white" />
                   <path d="M30.9469 12.4611L29.0987 14.3093C28.3779 15.0301 28.3733 16.1991 29.0987 16.9245C29.4591 17.2849 29.935 17.4652 30.4063 17.4652C30.8776 17.4652 31.3489 17.2849 31.7139 16.9245L33.5621 15.0763C34.2829 14.3555 34.2875 13.1865 33.5621 12.4611C32.8413 11.7403 31.6723 11.7357 30.9469 12.4611Z" fill="white" />
                 </svg>
-              </Link>
-            </motion.div>
+              </div>
+            </Link>
 
           </div>
 

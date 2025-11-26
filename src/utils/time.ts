2b190d2e1437bdfb72bff71fa5f1cdcb56ae7517
Supriyo Mahato <mesupriyomahato@gmail.com>
2025-11-26 22:33:04 +0530

    updatde post ui

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -3,20 +3,20 @@ export function formatTime(date: string | Date): string {
     const created = new Date(date);
     const diff = (now.getTime() - created.getTime()) / 1000; // seconds
 
-    if (diff < 60) return "Posted now";
+    if (diff < 60) return "now";
 
     const minutes = Math.floor(diff / 60);
-    if (minutes < 60) return `Posted ${minutes} min ago`;
+    if (minutes < 60) return `${minutes}min `;
 
     const hours = Math.floor(minutes / 60);
-    if (hours < 24) return `Posted ${hours} hr${hours === 1 ? "" : "s"} ago`;
+    if (hours < 24) return `${hours}hr `;
 
     const days = Math.floor(hours / 24);
-    if (days < 7) return `Posted ${days} day${days === 1 ? "" : "s"} ago`;
+    if (days < 7) return `${days}d `;
 
     const weeks = Math.floor(days / 7);
-    if (weeks < 50) return `Posted ${weeks} week${weeks === 1 ? "" : "s"} ago`;
+    if (weeks < 50) return `${weeks}w `;
 
     const years = Math.floor(weeks / 50);
-    return `Posted ${years} year${years === 1 ? "" : "s"} ago`;
+    return `${years}y `;
 }
